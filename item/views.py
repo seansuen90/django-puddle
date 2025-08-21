7fa7d23e6b87b3494548d7cb9600d1ab650a7c1a
seansuen90 <seansuen90@gmail.com>
2025-08-21 10:35:35 +0000

    about to impl detail.html

--- item/views.py
+++ item/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Item
+
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'item/detail.html', {
+        'item': item
+    })
--- puddle/urls.py
+++ puddle/urls.py
@@ -1,5 +1,6 @@
 
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -10,4 +11,8 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
+    path('items/', include('item.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
