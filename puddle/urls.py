fcb6b4447cf2b97502164561594426eb133dad63
seansuen90 <seansuen90@gmail.com>
2025-08-22 11:33:05 +0000

    added inbox and dashboard for auth user

--- puddle/urls.py
+++ puddle/urls.py
@@ -4,13 +4,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-from core.views import index, contact
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
+    path('', include('core.urls')),
     path('items/', include('item.urls'))
 ]
 
