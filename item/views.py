4a9732ec7f665ebcf4dd4418621a0bb0f7e289ef
seansuen90 <seansuen90@gmail.com>
2025-08-24 11:14:07 +0000

    added category query

--- item/views.py
+++ item/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import get_object_or_404, redirect, render
-
-from .models import Item
+from django.db.models import Q
+from .models import Category, Item
 from django.contrib.auth.decorators import login_required
 from .forms import NewItemForm, EditItemForm
 
 def items(request):
+    query = request.GET.get('query', '')
     items = Item.objects.filter(is_sold=False)
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    if category_id:
+        items = items.filter(category_id=category_id)
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    
     return render(request, 'item/items.html', {
-        'items': items
+        'items': items,
+        'query': query,
+        'categories': categories, 
+        'category_id': int(category_id)
     })
 
 
